1f37dd750ec99da39999b6ebb861b8b35f604191
ManuFlosoYT <manubh94y@gmail.com>
2024-09-23 13:27:24 +0200

    ej3

--- Introduccion a la programacion/Practica 2/ej3.cpp
+++ Introduccion a la programacion/Practica 2/ej3.cpp
@@ -2,13 +2,47 @@
 
 using namespace std;
 
+int metodo1()
+{
+	int bit0, bit1, bit2, bit3;
+	char cbit0, cbit1, cbit2, cbit3;
+	int decimal;
+	
+	cout << "Introduce 4 cifras en binario: ";
+	cin >> cbit3 >> cbit2 >> cbit1 >> cbit0;
+	
+	//Se resta el caracter 0 para sacar la diferencia entre los numeros al convertirlos de CHAR a INT
+	bit0 = (int)cbit0 - '0';
+	bit1 = (int)cbit1 - '0';
+	bit2 = (int)cbit2 - '0';
+	bit3 = (int)cbit3 - '0';
+		
+	decimal = bit0 + bit1*2 + bit2*4 + bit3*8;
+	
+	cout << "El valor decimal es: " << decimal;
+	return 0;
+}
+
+int metodo2()
+{
+	int bit0, bit1, bit2, bit3;
+	int decimal, binario;
+	
+	cout << "Introduce 4 cifras en binario: ";
+	cin >> binario;
+	
+	bit3 = binario / 1000;
+	bit2 = (binario % 1000) / 100;
+	bit1 = (binario % 100) / 10;
+	bit0 = binario % 10;
+		
+	decimal = bit0 + bit1*2 + bit2*4 + bit3*8;
+	
+	cout << "El valor decimal es: " << decimal;
+	return 0;
+}
+
 int main()
 {
-	char a, b, c, d;
-	cin >> a >> b >> c >> d;
-	int a1 = a;
-	int b1 = b;
-	int c1 = c;
-	int d1 = d;
-	cout << a1*8 << b1*4 << c1*2 << d1;
+	return metodo2();
 }
