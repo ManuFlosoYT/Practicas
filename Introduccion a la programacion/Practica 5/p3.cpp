22eb481b8de2faecea66e331842905e4965ddcb0
ManuFlosoYT <manubh94y@gmail.com>
2024-10-28 12:58:35 +0100

    tarea07

---
+++ Introduccion a la programacion/Practica 5/p3.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std; 
+
+int main () 
+{ 
+    int dividendo, divisor, cociente, resto;
+
+    cout << "Introduce el dividendo: ";
+    cin >> dividendo;
+    cout << "Introduce el divisor: ";
+    cin >> divisor;
+
+    
+    return 0; 
+}
